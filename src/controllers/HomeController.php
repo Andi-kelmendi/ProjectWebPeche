3c87e4a26c7cde3773c1b63a8de973dc6010913e
ak_alb0 <andi-kelmendi@hotmail.com>
2026-05-26 17:16:16 +0200

    Structure de projet / page Home

---
+++ src/controllers/HomeController.php
@@ -0,0 +1,8 @@
+<?php
+namespace Andi\WebPChe\Controllers;
+
+class HomeController {
+    public function index() {
+        require '../src/Views/home.php';
+    }
+}
---
+++ src/views/home.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/home.css">
+    <title>WebPêche</title>
+</head>
+<body>
+
+    <!-- NAVBAR -->
+    <nav>
+        <a href="/" class="nav-logo">
+        <img src="/images/logo.png" alt="WebPêche" width="50">
+        </a>
+        <div class="liens">
+            <ul class="nav-links">
+                <li><a href="/">Accueil</a></li>
+                <li><a href="/carte">Carte</a></li>
+                <li><a href="/support">Support</a></li>
+            </ul>
+            <a href="/login" class="btn-connect">Se connecter</a>
+        </div>
+    </nav>
+
+    <!-- HERO -->
+    <section class="hero">
+        <!-- TEXTE GAUCHE -->
+        <div class="hero-text">
+            <h1>Découvrez les Meilleurs endroits de Pêche</h1>
+            <p>Partagez vos expériences, trouvez les meilleurs endroits de pêche et connectez-vous avec une communauté passionnée de pêche.</p>
+            <div class="hero-buttons">
+                <a href="/spots" class="btn-secondary">Explorer les spots</a>
+                <a href="/register" class="btn-primary">Rejoindre la Communauté</a>
+            </div>
+        </div>
+
+        <!-- IMAGE (2 téléphones) -->
+        <div class="hero-image">
+            <img src="/images/phone.png" alt="Aperçu de l'application">
+        </div>
+    </section>
+
+</body>
+</html>
